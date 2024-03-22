08792bf8ede0ebf63cb11115f167d4a5c04a9ad7
UNEEZA ISMAIL <160355516+uneezaismail@users.noreply.github.com>
2024-03-22 04:17:03 -0700

    simple calculator

---
+++ eza-simple-calculator/index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter first Number",
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: "Enter second Number",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log("Your Answer is : ", answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log("Your Answer is : ", answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log("Your Answer is : ", answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log("Your Answer is : ", answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("please select valid operator");
+}
