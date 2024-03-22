ba161ef11844b257ce643225721f0f237603c1f0
UNEEZA ISMAIL <160355516+uneezaismail@users.noreply.github.com>
2024-03-22 04:14:15 -0700

    addition calculator

---
+++ eza-addition-calculator/index.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+const input1 = await inquirer.prompt({
+    name : "num1",
+    type : "number",
+    message : "Kindly enter your first number :",
+});
+
+const input2 = await inquirer.prompt({
+    name : "num2",
+    type : "number",
+    message : "Kindly enter your second number :",
+});
+
+let Total: number = input1.num1 + input2.num2;
+console.log(`The sum of your numbers is : ${Total}`);
