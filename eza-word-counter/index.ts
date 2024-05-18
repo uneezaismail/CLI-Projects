77feb96377fe6aa2f7cdcb556f42be6a5ebb7aae
UNEEZA ISMAIL <160355516+uneezaismail@users.noreply.github.com>
2024-05-18 07:50:06 -0700

    updated

---
+++ eza-word-counter/index.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+
+let condition = true;
+while (condition) {
+  const answer: {
+    sentence: string;
+  } = await inquirer.prompt([
+    {
+      name: "sentence",
+      type: " input",
+      message: "Please enter a sentence to count the words: ",
+    },
+  ]);
+
+  const words = answer.sentence.trim().split(" ");
+
+  console.log("Words in your sentence:", words);
+
+  console.log(
+    `The total counted word in your sentence is/are: ${words.length}`
+  );
+
+  let askUser = await inquirer.prompt({
+    name: "ask",
+    type: "confirm",
+    message: "Do you wish to analyze another sentence?",
+  });
+
+  condition = askUser.ask;
+  if (!condition) {
+    console.log("Exiting the program....");
+    console.log(`\x1b[1mThank you!\x1b[0m`);
+  }
+}
